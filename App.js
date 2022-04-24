b0080b2772b979af49db9c70a844a21df133e557
FrankApiyo <franklineapiyo@gmail.com>
2022-04-24 13:23:11 +0300

    Use images to mimic video player

--- App.js
+++ App.js
@@ -1,44 +1,95 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, StyleSheet, View, Image, ImageBackground} from 'react-native';
+
+const playIcon = require('./Images/play.png');
+const volumeIcon = require('./Images/sound.png');
+const hdIcon = require('./Images/hd-sign.png');
+const fullScreenIcon = require('./Images/full-screen.png');
+const flower = require('./Images/flower.jpg');
+const remoteImage = {
+  uri: 'https://farm5.staticflickr.com/4702/24825836327_bb2e0fc39b_b.jpg',
+};
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 10,
-    marginTop: 100,
-    backgroundColor: '#e67e22',
+  flower: {
+    flex: 1,
+  },
+  appContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    padding: 0,
+  },
+  videoContainer: {
+    backgroundColor: '#000',
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    marginLeft: 0,
+    width: '100%',
+  },
+  controlsContainer: {
+    padding: 10,
+    backgroundColor: '#202020',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 300,
+    width: '100%',
+    flex: 1,
+    borderWidth: 2,
+    display: 'flex',
+  },
+  icon: {
+    tintColor: 'powderblue',
+    height: 16,
+    width: 16,
+    marginRight: 5,
+    flex: 1,
+    flexGrow: 0.5,
+    borderWidth: 2,
+    marginLeft: 5,
+  },
+  progress: {
+    backgroundColor: '#000',
+    borderRadius: 7,
+    flex: 15,
+    flexBasis: 10,
+    display: 'flex',
+    borderWidth: 2,
+  },
+  progressBar: {
+    backgroundColor: '#bf161c',
     borderRadius: 5,
-  },
-  innerContainer: {
-    backgroundColor: '#d35400',
-    height: 50,
-    width: 150,
-    borderTopLeftRadius: 5,
-    borderBottomLeftRadius: 5,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '200',
-    color: '#fff',
-    position: 'absolute',
-    // backgroundColor: 'transparent',
-    top: 12,
-    left: 10,
-  },
-  subtitle: {
-    fontWeight: 'bold',
+    height: 10,
+    margin: 2,
+    paddingTop: 3,
+    width: '50%',
+    borderWidth: 2,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
-
 export default class App extends React.Component {
   render() {
-    const name = '01 - Blue Behind Green Bloches';
-
     return (
-      <View style={styles.container}>
-        <View style={styles.innerContainer} />
-        <Text style={styles.title}>
-          <Text style={styles.subtitle}>Playing:</Text> {name}
-        </Text>
+      <View style={styles.appContainer}>
+        <ImageBackground
+          source={remoteImage}
+          style={styles.videoContainer}
+          resizeMode="contain">
+          <View style={styles.controlsContainer}>
+            <Image source={volumeIcon} style={styles.icon} />
+            <View style={styles.progress}>
+              <View style={styles.progressBar} />
+            </View>
+            <Image source={hdIcon} style={styles.icon} />
+            <Image source={fullScreenIcon} style={styles.icon} />
+          </View>
+        </ImageBackground>
       </View>
     );
   }
